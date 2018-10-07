5341043d4ffd618bfa1bbf94f89e616f4a801a14
Diego <dhernandezvelazquez@my.milligan.edu>
2018-10-07 19:25:28 -0400

    Testing posts

--- bufflog/src/database.js
+++ bufflog/src/database.js
@@ -4,6 +4,7 @@ export default {
   Posts: [
     new blogPost("Sample post", "Bradley Hammond", "October 10th, 2018", "Sample Body..."),
     new blogPost("Sample post", "Bradley Hammond", "October 10th, 2018", "Sample Body...")
+	new blogPost("Not Sample post", "Diego Hernandez", "October 31, 2018", "I am in the future...")
   ],
   ContactRequests: [],
   User: []
