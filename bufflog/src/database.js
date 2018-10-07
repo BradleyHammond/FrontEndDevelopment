13771be9e9cfa59f452308a4c40b517bf51693bd
Diego <dhernandezvelazquez@my.milligan.edu>
2018-10-07 19:26:44 -0400

    Revert "Testing posts"
    This reverts commit 5341043d4ffd618bfa1bbf94f89e616f4a801a14.

--- bufflog/src/database.js
+++ bufflog/src/database.js
@@ -4,7 +4,6 @@ export default {
   Posts: [
     new blogPost("Sample post", "Bradley Hammond", "October 10th, 2018", "Sample Body..."),
     new blogPost("Sample post", "Bradley Hammond", "October 10th, 2018", "Sample Body...")
-	new blogPost("Not Sample post", "Diego Hernandez", "October 31, 2018", "I am in the future...")
   ],
   ContactRequests: [],
   User: []
