89b51233e029e0e582e005537852ab4a991b838f
Bradley Hammmond <Hammondbv@gmail.com>
2018-10-07 16:49:17 -0400

    Added blogpost

---
+++ bufflog/src/blogpost.js
@@ -0,0 +1,9 @@
+export default class Blogpost {
+    constructor(id, title, author, date, body){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.body = body;
+    }
+}
