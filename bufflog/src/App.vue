cf25a4f5925fa6a3230ca06d05dbc75ed2ad9f08
Bradley Hammmond <Hammondbv@gmail.com>
2018-10-07 16:49:50 -0400

    styling changes

--- bufflog/src/App.vue
+++ bufflog/src/App.vue
@@ -24,8 +24,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-#header{
-  text-align: left;
-  
-}
+
 </style>
--- bufflog/src/components/Header.vue
+++ bufflog/src/components/Header.vue
@@ -35,11 +35,12 @@
 #header {
     position: absolute;
     width: 100%;
-    height: 50px;
+    height: 100px;
     background-color: orangered;
     top: 0;
     left: 0;
-    padding: 25px;
+    padding: 0px;
+    text-align: right;
     
 }
 
