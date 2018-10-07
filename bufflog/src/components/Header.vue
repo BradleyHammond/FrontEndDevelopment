957e0f0af79a7f0f0e558c9521cad1f0f9083af4
Bradley Hammmond <Hammondbv@gmail.com>
2018-10-07 15:42:39 -0400

    Added Header File

---
+++ bufflog/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id = "header">
+        <span v-html="createPost" id="create-post-link"></span>
+        <span v-html="contactRequests" id="contact-request-link"></span>
+        <input type="button" v-model="msg" @click="clicking()" />
+    </div>
+</template>
+
+<script>
+    export default{
+    data() {
+        return{
+            msg: "Login",
+            createPost: '',
+            contactrequests: ''
+        };
+    },
+    methods: {
+        clicking(){
+            if (this.msg == "Login"){
+                this.msg = "Sign Out"
+                this.createPost = '<a href="#">Create Post</a>';
+            } else{
+            this.msg = "Login";
+            this.createPost = '';
+            this.contactrequests = '';
+            }
+        }
+    }
+
+}
